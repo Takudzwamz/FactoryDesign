3aeb967e7e4a5f4cfae05bbd69c801e8da050da5
Takudzwamz <zhout1709@hotmail.com>
2020-03-31 20:26:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace FactoryDesign
+{
+    //Creating Product Interface
+    public interface CreditCard 
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+
+    // Creating Concrete Products
+    public class Platinum : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 50000;
+        }
+
+        public int GetAnnualCharge()
+        {
+            return 15000;
+        }
+    }
+
+    public class Titanium : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class MoneyBack : CreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+    // Creating Abstract Creator
+    public abstract class CreditCardFactory
+    {
+        protected abstract CreditCard MakeProduct();
+        public CreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+    //Creating Concrete Creator
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override CreditCard MakeProduct()
+        {
+            CreditCard product = new Titanium();
+            return product;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+}
+
